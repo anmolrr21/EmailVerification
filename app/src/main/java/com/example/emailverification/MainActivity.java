8cba7e4714d034ab580391051a3c0b6d6f84f245
anmolrr21 <varsharohra3april@gmail.com>
2019-12-23 16:45:08 +0530

    ANMOL

---
+++ app/src/main/java/com/example/emailverification/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.emailverification;
+
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1,e2,e3;
+    Button b1;
+
+
+    DataHelper db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1 = findViewById(R.id.email);
+        e2 = findViewById(R.id.pass);
+        e3 = findViewById(R.id.cpass);
+        b1 = findViewById(R.id.register);
+
+        final String emailPattern = "(.+)\\.+[a-zA-Z0-9._-]+@(.+)$";
+        //below pattern says 1 digit,uppercase,lowercase,no whitespace,atleast 8 chars
+       // final String passpattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,10}";
+
+        db = new DataHelper(this);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1 = e1.getText().toString();
+                String s2 = e2.getText().toString();
+                String s3 = e3.getText().toString();
+
+
+                if (s1.equals("") || s2.equals("") || s3.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Fields can't be empty", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    if (s2.equals(s3)){
+                            //&&(e2.getText().toString().trim().matches(passpattern))) {
+
+
+                        if ((e1.getText().toString().trim().matches(emailPattern))&&(e1.getText().toString().endsWith("ves.ac.in"))&&(e1.getText().toString().startsWith("2018"))){
+                            // Toast.makeText(getApplicationContext(),"valid email address",Toast.LENGTH_SHORT).show();
+
+                            Boolean chkemail = db.chkemail(s1);
+                            if (chkemail == true) {
+                                Boolean insert = db.insert(s1, s2);
+                                if (insert == true) {
+                                    Toast.makeText(getApplicationContext(), "Registered successfully", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Email already exists", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Enter only ves id", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Invalid password", Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+
+            }
+        });
+
+
+        Button btn = (Button) findViewById(R.id.login);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            //            SQLiteDatabase db=this.getWriteableDatabase();
+            @Override
+            public void onClick(View v) {
+
+//                Cursor cursor=db.rawQuery("Select * from user where email=?",new String[]{email});
+//                db.exec
+                DataHelper o = new DataHelper(getApplicationContext());
+                if(!o.chkemail(e1.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Logged in sucessfully", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(MainActivity.this, mylogin.class));
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Not registered" +
+                            "", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+
+
+        });
+    }}
---
+++ app/src/main/java/com/example/emailverification/mylogin.java
@@ -0,0 +1,59 @@
+package com.example.emailverification;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class mylogin extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        Button b3 = (Button) findViewById(R.id.logout);
+        b3.setOnClickListener(this);
+
+        Button b4 = (Button) findViewById(R.id.fpass);
+        b4.setOnClickListener(this);
+
+
+        //String s=findViewById(R.id.fpass).toString();
+
+
+
+    }
+
+    //if clicked on logout will go to the first page
+    public void onClick(View view) {
+
+        Button b3 = (Button) findViewById(R.id.logout);
+        Button b4=(Button)findViewById(R.id.fpass);
+
+        switch (view.getId()) {
+//
+            case R.id.logout:
+                b3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(mylogin.this, MainActivity.class));
+                    }
+                });
+
+
+                break;
+
+            case R.id.fpass:
+
+//                b4.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        startActivity(new Intent(mylogin.this, factivity.class));
+//                    }
+//                });
+//                break;}
+//    }
+}}}
